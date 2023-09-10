f051d8473927d72e392701f2deb21469341b1210
delia <you@example.com>
2023-09-09 21:23:48 -0400

    import routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from 'apollo/link/context';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import './components/styles.css';
+
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import MyPlans from './components/MyPlans';
+import AllPlans from './components/AllPlans';
+import UpdatePlan from './components/UpdatePlan';
+import CreatePlan from './components/CreatePlan';
+import PlanDetails from './components/PlanDetails';
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:5000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+    return {
+      <ApolloProvider client={client}>
+        <Router>
+          <div className='flex-column justify-flex-start min-100-vh'>
+            <Navbar />
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/myPlans' element={<MyPlans />} />
+              <Route path='/allPlans' element={<AllPlans />} />
+              <Route path='/createPlan' element={<CreatePlan />} />
+              <Route path='updatePlan/:id' element={<UpdatePlan />} />
+              <Route path='planDetails/:id' element={<PlanDetails />} />
+              <Route
+                path='*'
+                element={<h1 className='display-2'>Wrong page!</h1>}
+              />
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      </ApolloProvider>
+    };
+    }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
